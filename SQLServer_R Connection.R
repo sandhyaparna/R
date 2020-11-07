70c4b262f7726f6fdcb1f897e61fa4ae231688c2
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2020-11-06 19:54:34 -0600

    Delete SQLServer_R Connection.R

--- SQLServer_R Connection.R
+++
@@ -1,5 +0,0 @@
-library(RODBC)
-cn <-odbcDriverConnect(connection="Driver={SQL Server Native Client 11.0};server=vigi-dw01;database=Datawarehouse;trusted_connection=yes;")
-res <- sqlQuery(cn, 'select * from [DataWarehouse].[dbo].[DimDate]')
-
-
